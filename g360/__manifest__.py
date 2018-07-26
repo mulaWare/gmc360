ea953ea8f26d83082748e48ca84a7c5516f2db96
Luis Vazquez <luis.vzqz@Luis.Home>
2018-07-26 14:57:44 -0500

    Modification pf GMC360

--- g360/__manifest__.py
+++ g360/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "g360",
+    'name': "GMC360",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+                Customization for GMC360
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Customization for GMC360
+    """,
+
+    'author': "luisvzqz",
+    'website': "https://www.gmc360.com.mx",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'addons',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
