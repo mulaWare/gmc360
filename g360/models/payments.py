6571036ae30c995dc55c3871104696767195eddf
Luis Vazquez <luis.vzqz@mulaware.com>
2018-10-23 19:02:26 -0500

    Update payments.py

--- g360/models/payments.py
+++ g360/models/payments.py
@@ -34,7 +34,7 @@ class AccountPayment(models.Model):
         '''
         if not hasattr(cfdi, 'Complemento'):
             return None
-        attribute = '//pago10:Pago'
+        attribute = '//pago10:Pagos'
         namespace = {'pago10': 'http://www.sat.gob.mx/Pagos'}
         node = cfdi.Complemento.xpath(attribute, namespaces=namespace)
         return node
