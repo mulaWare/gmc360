6318f82a19d6e9ecf4a5ad2ec80af3b7671b9426
MarioEst <mario@gmc360.com.mx>
2019-04-16 16:05:46 -0500

    MARK 28

--- g360/models/sale.py
+++ g360/models/sale.py
@@ -4,7 +4,7 @@ import urllib.parse
 import requests
 import datetime
 
-class SaleOrder(models.Model):
+class SalePrueba(models.Model):
     _inherit = "sale.order"
 
     fecha_probable = fields.Date(string="Fecha Probable de cierre")
