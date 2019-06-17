308e945457565a87877edb96d272ae5471beedf9
Luis Vazquez <luis.vzqz@mulaware.com>
2019-06-17 18:35:31 -0500

    Update sale.py

--- g360/models/sale.py
+++ g360/models/sale.py
@@ -4,7 +4,7 @@ import urllib.parse
 import requests
 import datetime
 
-class SalePrueba(models.Model):
+class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     fecha_probable = fields.Date(string="Fecha Probable de cierre")
