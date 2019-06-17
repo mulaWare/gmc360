fa7f8425bf3af70aa507495cab15b3b1341552be
Luis Vazquez <luis.vzqz@mulaware.com>
2019-06-17 18:24:20 -0500

    Update sale.py

--- g360/models/sale.py
+++ g360/models/sale.py
@@ -8,3 +8,10 @@ class SalePrueba(models.Model):
     _inherit = "sale.order"
 
     fecha_probable = fields.Date(string="Fecha Probable de cierre")
+    
+    @api.multi
+    def unlink(self):
+        for order in self:
+            if order.state not in ('draft',):
+                raise UserError(_('You can not delete a sent quotation or a confirmed sales order. You must first cancel it.'))
+        return super(SaleOrder, self).unlink()
