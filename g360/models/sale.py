b46172ae10ae6d2a7b7e34061ba069afc702df94
Luis Jaramillo <luis@gmc360.com.mx>
2019-08-16 10:56:51 -0500

    Mark 34

--- g360/models/sale.py
+++ g360/models/sale.py
@@ -8,24 +8,24 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     fecha_probable = fields.Date(string="Fecha Probable de cierre")
-    plazo = fields.Char(string="Plazo")
-    anticipo = fields.Char(string="% de anticipo")
-    precio_total = fields.Char(string="Precio Total")
+    plazo = fields.Float(string="Plazo")
+    anticipo = fields.Float(string="% de anticipo")
+    precio_total = fields.Float(string="Precio Total")
     fecha_pago = fields.Date(string="Fecha de Pago")
-    pago_total = fields.Char(string="Pago Total")
-    pago_contado = fields.Char(string="Pago Contado")
-    pago_anticipo = fields.Char(string="Pago Anticipo")
+    pago_total = fields.Float(string="Pago Total")
+    pago_contado = fields.Float(string="Pago Contado")
+    pago_anticipo = fields.Float(string="Pago Anticipo")
     pago_iguala = fields.Char(string="Pago Iguala")
-    nombre = fields.Char(string="Nombre del Tesorero")
-    puesto = fields.Char(string="Puesto del Tesorero")
-    telefono = fields.Char(string="Telefono del Tesorero")
-    correo = fields.Char(string="Correo del Tesorero")
     metodo = fields.Selection([
                                 ('00','Tranferencia'),
                                 ('01','Deposito a cuenta'),
                                 ('02','Pago Electronico'),
                                 ],
                                 string="Metodo de pago", required=False, help="Metodo de pago")
+    nombre = fields.Char(string="Nombre del Tesorero")
+    puesto = fields.Char(string="Puesto del Tesorero")
+    telefono = fields.Integer(string="Telefono del Tesorero")
+    correo = fields.Char(string="Correo del Tesorero")
 
     @api.multi
     def unlink(self):
