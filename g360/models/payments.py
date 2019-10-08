d224ad5a9378ff552e291fafb81ca8eae2088b36
Luis Vazquez <luis.vzqz@mulaware.com>
2019-10-08 12:52:40 -0500

    Update payments.py

--- g360/models/payments.py
+++ g360/models/payments.py
@@ -35,4 +35,4 @@ class AccountPayment(models.Model):
         attribute = '//pago10:Pago[1]'
         namespace = {'pago10': 'http://www.sat.gob.mx/Pagos'}
         node = cfdi.Complemento.xpath(attribute, namespaces=namespace)
-        return node[0] if node else None
+        return node[0] if node else None 
