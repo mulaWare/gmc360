25ed05246b931bdcc6661b424a8e8b009ade1c00
Luis Jaramillo <luis@gmc360.com.mx>
2019-10-10 12:04:01 -0500

    Mark 03

--- g360/__manifest__.py
+++ g360/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','l10n_mx_edi',],
+    'depends': ['base','account','l10n_mx_edi','crm',],
 
     # always loaded
     'data': [
