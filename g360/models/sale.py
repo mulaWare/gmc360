2768558c413489035aeef4d6be024c78152c9b03
luisvzqz <luis.vzqz@mulaware.com>
2020-05-13 12:05:38 -0500

    ADD sale-sign

--- g360/models/sale.py
+++ g360/models/sale.py
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
 
 
         sign1_name = self.sign_reference + '_sign1'
-        sign1_type_id = self.env['sign.item.type'].search(['type','=','signature'], limit=1).id
+        sign1_type_id = self.env['sign.item.type'].search([('type','=','signature')], limit=1).id
         sign1_responsible_id = self.env['sign.item.role'].search([], limit=1).id
         sign1 = self.env['sign.item'].create({
                                         'template_id': sign_template_id.id,
