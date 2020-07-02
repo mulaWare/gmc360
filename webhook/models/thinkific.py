4c8715ac624ddab9a66fd5167a856930f4cdd01b
luisvzqz <luis.vzqz@mulaware.com>
2020-07-01 20:50:20 -0500

    [FIX] webhook add last_request

--- webhook/models/thinkific.py
+++ webhook/models/thinkific.py
@@ -18,9 +18,12 @@ PORT = '8069'
 class Webhook(models.Model):
     _inherit = 'webhook'
 
+    last_request = fields.Text(string="Last Request")
+
     @api.multi
     def run_thinkific_order_created(self):
         self.ensure_one()
+        self.last_request = self.env.request.jsonrequest
         pprint.pformat(self.env.request.jsonrequest)[:450]
         if self.env.request.jsonrequest['foo'] != 'bar':
             raise exceptions.ValidationError(_("Wrong value received"))
